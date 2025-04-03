a45c1daa79af43f208ae486cb4408b933dd90d54
imjamesonzeller <imjamesonzeller@gmail.com>
2025-04-03 11:59:44 -0500

    Placement Information to Dataclass

--- models/PlacementInformation.py
+++ models/PlacementInformation.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
 class PlacementInformation():
-    def __init__(self, word, coords, direction):
-        self.word: str = word
-        self.coords: tuple[int, int] = coords
-        self.direction: dict[str, int] = direction
+    word: str
+    coords: tuple[int, int]
+    direction: dict[str, int]
